d5d32171944d09044905768378b9595bc12e7b8c
CharleyRog <k.ichko.ua@gmail.com>
2023-03-07 23:59:15 +0200

    Uncommented tests

--- src/tests/user.test.ts
+++ src/tests/user.test.ts
@@ -1,25 +1,25 @@
-// import test from 'node:test'
-// import getUsers from '../helpers/getUsers.js'
-// import getUser from '../helpers/getUser.js'
+import test from 'node:test'
+import getUsers from '../helpers/getUsers.js'
+import getUser from '../helpers/getUser.js'
 
-// test('getUsers should return an array', async () => {
-//   const users = await getUsers()
-//   expect(Array.isArray(users)).toBe(true)
-// })
-//
-// test('saveUser should add a user to the file', async () => {
-//   const user = { chat_id: 123456, city: 'Moscow' }
-//   const users = await getUsers()
-//   expect(users).toContainEqual(user)
-// })
-//
-// test('getUser should return a user if it exists', async () => {
-//   const user = { chat_id: 123456, city: 'Moscow' }
-//   const result = await getUser(user.chat_id)
-//   expect(result).toEqual(user)
-// })
-//
-// test('getUser should return undefined if user does not exist', async () => {
-//   const result = await getUser(123456)
-//   expect(result).toBeUndefined()
-// })
+test('getUsers should return an array', async () => {
+  const users = await getUsers()
+  expect(Array.isArray(users)).toBe(true)
+})
+
+test('saveUser should add a user to the file', async () => {
+  const user = { chat_id: 123456, city: 'Moscow' }
+  const users = await getUsers()
+  expect(users).toContainEqual(user)
+})
+
+test('getUser should return a user if it exists', async () => {
+  const user = { chat_id: 123456, city: 'Moscow' }
+  const result = await getUser(user.chat_id)
+  expect(result).toEqual(user)
+})
+
+test('getUser should return undefined if user does not exist', async () => {
+  const result = await getUser(123456)
+  expect(result).toBeUndefined()
+})
